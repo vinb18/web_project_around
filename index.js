19ea62b8ef7b18f7c15600ada0a89f002a598fd6
Veronica Itzel Netba <veronica.netba@gmail.com>
2023-11-29 01:02:49 -0600

    JS buttons

--- index.js
+++ index.js
@@ -1,43 +1,3 @@
-/*function hacerClick() {
-    console.log("hice click")
-}
-// DOM
-// 1. DOCUMENT OBJECT MODEL
-
-// A. SELECCIÓN
-const buttons = document.querySelectorAll(".button")
-// buttons[0] => button_edit
-
-const modal = document.querySelector(".popup")
-const closeModalBtn = document.querySelector(".button__close")
-console.log(closeModalBtn)
-
-// B. MANIPULACIÓN
-// I. ACTIVACIÓN DE MODAL PARA EDITAR DE PERFIL
-buttons[0].addEventListener("click", doClick)
-
-buttons[0].addEventListener("click", hacerClick)
-// II. CERRAR MODAL
-closeModalBtn.addEventListener("click", closePopup)
-
-// 2. FUNCIONES DECLARADAS - ACCIONES
-function doClick() {
-  // 1. SELECCIONAR MODAL. (YA ESTÁ HECHO)
-  // 2. ACTIVAR MODAL (CLASE EN CSS .popup) CAMBIANDO EL DISPLAY NONE Aº DISPLAY BLOCK
-  modal.style.display = "block"
-  return
-}
-
-for (let i=0; i < buttons.length; i++){
-    console.log(buttons[i]);
-}
-function closePopup() {
-  console.log("cerrando popup")
-  // 1. SELECCIONAR MODAL (YA ESTÁ HECHO)
-  // 2. DESACTIVAR MODAL (CLASE EN CSS .popup) CAMBIANDO EL DISPLAY DE NONE A BLOCK
-  modal.style.display = "none"
-}*/
-
 const buttonEdit = document.querySelector(".button__edit");
 const popupProfile = document.querySelector(".popup");
 const buttonClose = document.querySelector(".button__close");
@@ -45,3 +5,25 @@ const buttonClose = document.querySelector(".button__close");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 const profileForm = document.querySelector(".popup__form");
+const inputName = document.querySelector(".popup__item_name")
+const inputJob = document.querySelector(".popup__item_job")
+
+function togglePopup(popup){
+    popup.classList.toggle("popup__opened");
+
+    }
+
+buttonEdit.addEventListener("click",function(){
+    togglePopup(popupProfile);
+});
+
+buttonClose.addEventListener("click",function(){
+    togglePopup(popupProfile);
+});
+
+profileForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    togglePopup(popupProfile)
+})
