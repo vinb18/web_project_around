54f5816ad356dab35632786b7259856112826468
Veronica Itzel Netba <veronica.netba@gmail.com>
2024-02-01 14:04:40 -0600

    Section

---
+++ scripts/Section.js
@@ -0,0 +1,11 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  addItem(cardElement) {
+    this._container.appendChild(cardElement);
+  }
+}
